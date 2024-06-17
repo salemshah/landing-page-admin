5d00544e623e34c24b0bd8a3277793fa7b77f001
Salem Shah <salemshahdev@gmail.com>
2024-06-17 09:41:43 +0200

    LP-124 working on auth login

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -6,8 +6,7 @@ const axiosInstance = axios.create({
 });
 
 // Request interceptor
-axiosInstance.interceptors.request.use(
-  (config) => {
+axiosInstance.interceptors.request.use((config) => {
     // Add authorization token to headers if available
     const token = localStorage.getItem('token');
     if (token) {
@@ -25,7 +24,7 @@ axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && error.response.status === 401) {
-      window.location.href = '/login';
+      window.location.href = '/auth/login';
     }
     return Promise.reject(error);
   }
