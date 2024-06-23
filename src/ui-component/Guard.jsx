cf6a6890425f0d3fbe94e41f725f25a22f162a8d
Salem Shah <salemshahdev@gmail.com>
2024-06-23 22:28:56 +0200

    LP-143 fix the bugs login

--- src/ui-component/Guard.jsx
+++ src/ui-component/Guard.jsx
@@ -2,17 +2,18 @@
 import React, { useEffect, useState } from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
+import axios from 'axios';
+import axiosInstance from '../api/axiosInstance';
 
 const Guard = ({ children }) => {
 
   const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     setIsLoading(true);
-    const user = localStorage.getItem('user');
-    const token = localStorage.getItem('token');
-    if (!user || !token) window.location.href = '/auth/login';
-    setIsLoading(false);
-
+    axiosInstance.post('/auth/check-token').then().catch(console.error).finally(() => {
+      setIsLoading(false);
+    });
   }, []);
 
   return (
